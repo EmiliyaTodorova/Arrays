57d352d6c12d2d152ced83a0147433fd922f2eb0
User <User@DESKTOP-3CAS9B5>
2022-07-06 13:02:43 +0300

    Add project files.

---
+++ 03_Arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _03_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int n = int.Parse(Console.ReadLine());
+
+            int[] arr1 = new int[n];
+            int[] arr2 = new int[n];
+
+            for(int i = 1; i <= n; i++)
+            {
+                int[] currentRowData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                int firstNumber = currentRowData[0];
+                int secondNumber = currentRowData[1];
+                if (i % 2 != 0)
+                {
+                    arr1[i - 1] = firstNumber;
+                    arr2[i - 1] = secondNumber;
+                } else
+                {
+                    arr1[i - 1] = secondNumber;
+                    arr2[i - 1] = firstNumber;
+                }
+            }
+            Console.WriteLine(String.Join(" ", arr1));
+            Console.WriteLine(String.Join(" ", arr2));
+
+        }
+    }
+}
---
+++ 4_ArrayRotation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _4_ArrayRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int rotationCount = int.Parse(Console.ReadLine());
+            for(int i = 0; i < rotationCount; i++)
+            {
+                int firstElement = arr[0];
+                for(int j = 0; j <= arr.Length-2; j++)
+                {
+                    arr[j] = arr[j+1];
+                }
+                arr[arr.Length-1] = firstElement;   
+            }
+            Console.WriteLine(String.Join(" ", arr));
+        }
+    }
+}
